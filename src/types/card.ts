329fb2c7fd77ee5f7a8f81b8c44517bb3782e529
Manoj <manoj@Aruns-MacBook-Air.local>
2025-06-30 16:55:56 +0530

    fix: added missing types for Vercel deployment

---
+++ src/types/card.ts
@@ -0,0 +1,7 @@
+export type CreditCard = {
+  id: string
+  name: string
+  assetName: string
+  enabled: boolean
+  archived: boolean
+}
---
+++ src/types/ffp.ts
@@ -0,0 +1,9 @@
+export type FFP = {
+  id: string
+  name: string
+  assetName: string
+  enabled: boolean
+  archived: boolean
+  createdAt: string
+  updatedAt: string
+}
